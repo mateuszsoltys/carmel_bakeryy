8812af72d96e47e3c8492457ca56a254dcd6c218
Mateusz Soltys <admiral.soltys@google.com>
2022-08-14 15:19:15 +0200

    create data model for upcoming widgets infos

---
+++ lib/models/data/OfferDataModel.dart
@@ -0,0 +1,12 @@
+class OfferDataModel {
+  OfferDataModel(
+      {required this.Av_URL,
+      required this.Availability,
+      required this.Name,
+      required this.Price});
+
+  final String Av_URL;
+  final bool Availability;
+  final String Name;
+  final String Price;
+}
